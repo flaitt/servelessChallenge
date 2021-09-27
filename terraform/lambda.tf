03d9edbb641aabeba4b6b9622027a64b085925e3
Your Name <flaittj@gmail.com>
2021-09-27 20:24:41 -0300

    testing ci-cd

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -45,12 +45,12 @@ data "aws_iam_policy_document" "policy" {
 
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
-  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
 }
 
 data "aws_iam_policy_document" "create_policy" {
   statement {
-    effect   = "Allow"
+    effect    = "Allow"
     resources = ["*"]
     actions = [
       "dynamodb:ListTables",
@@ -58,7 +58,7 @@ data "aws_iam_policy_document" "create_policy" {
   }
 
   statement {
-    effect   = "Allow"
+    effect    = "Allow"
     resources = ["arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${aws_dynamodb_table.this.name}"]
     actions = [
       "dynamodb:PutItem",
@@ -73,13 +73,13 @@ data "aws_iam_policy_document" "create_policy" {
 }
 
 resource "aws_iam_policy" "create_policy" {
-  name = "lambda-policy"
+  name   = "lambda-policy"
   policy = data.aws_iam_policy_document.create_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "cat_policy" {
   policy_arn = aws_iam_policy.create_policy.arn
-  role = aws_iam_role.iam_for_lambda.name
+  role       = aws_iam_role.iam_for_lambda.name
 }
 
 
